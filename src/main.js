bff7c5b17e6fafa2d5c00610b0588d5a91bc31b7
apclarity <andreaspeterc@gmail.com>
2024-07-29 14:04:20 +0700

    fixing main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue'
-<<<<<<< HEAD
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
-=======
 import router from './router'
 import './style.css'
 import App from './App.vue'
@@ -14,4 +8,3 @@ const app = createApp(App);
 app.use(router)
 app.mount('#app')
 
->>>>>>> 96568f2... install vue router, learn component
